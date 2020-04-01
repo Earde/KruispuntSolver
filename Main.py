5ef341625d6eedf4bc422d560465628fb4bfa4c6
Unknown <sammiesamsam@live.nl>
2020-04-01 17:07:18 +0200

    Updated case, tried to fix socket connection

--- Main.py
+++ Main.py
@@ -2,34 +2,44 @@ from Crossroads.Crossroad import Crossroad
 from Solving.Solver import Solver
 from Network.Socket import Socket
 import time
+import asyncio
+import websockets
 import os
 
 clear = lambda: os.system('cls')
 
-socket = Socket("127.0.0.1")
-socket.connect()
-
 crossroad = Crossroad()
+socket = Socket(crossroad)
+
 solver = Solver(crossroad)
 solver.solve()
-solver.print()
+
+async def solve():
+    while True:
+        sleepTime = 1.0
+        solver.solve()
+        if crossroad.update(sleepTime):
+            await socket.send(crossroad.getJson())
+            
+            
+        crossroad.print()
+        await asyncio.sleep(sleepTime)
 
 #TODO: Handle websocket data
+async def start_server():
+    websockets.serve(socket.recieve, "localhost", 8000)
+    while True:
+        sleepTime = 1.0
+        solver.solve()
+        if crossroad.update(sleepTime):
+            await socket.send(crossroad.getJson())
+            
+            
+        crossroad.print()
+        await asyncio.sleep(sleepTime)
+        
+asyncio.get_event_loop().run_until_complete(start_server())
+asyncio.get_event_loop().run_forever()
 
-sleepTime = 1.0
-while (True):
-    clear()
 
-    obj = socket.receive()
-    if obj is not None:
-        crossroad.setQuantities(obj)
-
-    solver.solve()
-    if crossroad.update(sleepTime):
-        socket.send(crossroad.getJson())
-    
-    crossroad.print()
-    time.sleep(sleepTime)
-
-socket.close()
 print("done")
